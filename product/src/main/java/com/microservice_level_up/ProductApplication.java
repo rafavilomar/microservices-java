979de7527b1fcf71f6eda4acec45127f461c178d
Rafael Vilomar <rafavilomar@gmail.com>
2023-06-04 19:53:14 -0400

    feat: add new product microservice

---
+++ product/src/main/java/com/microservice_level_up/ProductApplication.java
@@ -0,0 +1,11 @@
+package com.microservice_level_up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+}
