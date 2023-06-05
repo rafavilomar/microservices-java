07b4c50899ba6d86978140bea7f0b39b2faa555c
Rafael Vilomar <rafavilomar@gmail.com>
2023-06-04 22:43:49 -0400

    feat: add Product entity

---
+++ product/src/main/java/com/microservice_level_up/module/product/Product.java
@@ -0,0 +1,43 @@
+package com.microservice_level_up.module.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @SequenceGenerator(name = "product_id_sequence", sequenceName = "product_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_id_sequence")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int stock;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
